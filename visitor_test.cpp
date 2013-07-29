517aedade670c8be248dd9de9ee680507cbac75e
ralphtandetzky <ralph.tandetzky@googlemail.com>
2013-07-29 17:22:17 +0200

    Create visitor_test.cpp

---
+++ visitor_test.cpp
@@ -0,0 +1,19 @@
+struct Client1; 
+struct Client2;
+typedef Visitor<Client1, Client2> BaseClientVisitor;
+struct Client1 : BaseClientVisitor::Visitable<Client1> { };
+struct Client2 : BaseClientVisitor::Visitable<Client2> { };
+
+struct Client2a;
+struct Client2b;
+typedef Visitor<Client2a, Client2b> Client2Visitor;
+struct Client2a : Client2, Client2Visitor::Visitable<Clien2a> { };
+struct Client2b : Client2, Client2Visitor::Visitable<Clien2b> { };
+
+struct AllClientsVisitor : BaseClientVisitor, Client2Visitor
+{
+  virtual void visit( Client2 & t )
+	{
+		t.accept( static_cast<Client2Visitor&>(*this) );
+	}
+};
