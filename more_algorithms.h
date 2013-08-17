31be4798d0e276fcf025f01fb7066e2e4d432d6d
Ralph Tandetzky <ralph.tandetzky@gmail.com>
2013-08-17 13:45:04 +0200

    Added moving_accumulate to more_algorithms.h

---
+++ more_algorithms.h
@@ -0,0 +1,24 @@
+#pragma once
+
+template <class InputIterator, class T, class BinOp>
+T moving_accumulate (
+        InputIterator first, InputIterator last, T init, BinOp binop)
+{
+  while ( first != last ) {
+    init = binop( std::move(init), *first );
+    ++first;
+  }
+  return init;
+}
+
+
+template <class InputIterator, class T, class BinOp>
+T moving_accumulate (
+        InputIterator first, InputIterator last, T init )
+{
+  while ( first != last ) {
+    init = std::move(init) + *first;
+    ++first;
+  }
+  return init;
+}
