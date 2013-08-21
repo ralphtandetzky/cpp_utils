c19151787fb6e9d7db81623416b8868a77936bcd
Ralph Tandetzky <ralph.tandetzky@gmail.com>
2013-08-21 19:46:54 +0200

    Added math_constants.h

---
+++ math_constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace cu
+{
+
+static const double pi    = 3.1415926535897932384626433;
+static const double e     = 2.7182818284590452353602874;
+static const double sqrt2 = 1.4142135623730950488016887;
+
+}
