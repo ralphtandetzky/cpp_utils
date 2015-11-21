61c6b2c271b78661a5663fade9c0ee476929fbe9
Matthias Klein <matthias.klein@optimeas.de>
2015-11-21 13:41:28 +0100

    initial commit

---
+++ include/om_cpp_utils/filesystem.hpp
@@ -0,0 +1,41 @@
+/** optiMEAS GmbH Measurement and Automation Systems
+ *  ------------------------------------------------
+ *
+ *  copright (c) 2015 -
+ *  author:      $Author$
+ *
+ *  e-Mail:      info@optimeas.de
+ *  web:         http://www.optimeas.de
+ *
+ *  This file is part of the project:
+ *
+ *      $Project$
+ *      ---------------------------------------------
+ *
+ * *************************************************************************
+ *
+ * The source code is given as is. The author is not responsible for any
+ * possible damage done due to the use of this code. The component can be
+ * freely used in any application. Source code remains property of the
+ * author and may not be distributed, published, given or sold in any
+ * form as such. No parts of the source code can be included in any other
+ * component or application without written authorization of optiMEAS GmbH.
+ *
+ * The documents version, history and changelog is provided from the
+ * version control system (SVN, GIT, etc.). It is never added to the
+ * document itself avoiding unnecessary changes and conflicts during
+ * merge.
+ *
+ * ************************************************************************
+ */
+
+#pragma once
+
+#include <om_cpp_utils/export.hpp>
+#include <string>
+
+namespace cu {
+
+bool fileExist(const std::string &fileName);
+
+} // namespace cu
