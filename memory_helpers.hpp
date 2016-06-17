76dafb1d20323dbde72e6888b08ec5d96c82791f
Ralph Tandetzky <ralph.tandetzky@optimeas.de>
2016-06-17 15:48:38 +0200

    Added to_unique_ptr() function.

--- memory_helpers.hpp
+++ memory_helpers.hpp
@@ -6,6 +6,12 @@ namespace cu
 {
 
 template <typename T>
+std::unique_ptr<std::decay_t<T>> to_unique_ptr( T && x )
+{
+  return std::make_unique<T>( std::forward<T>(x) );
+}
+
+template <typename T>
 std::shared_ptr<std::decay_t<T>> to_shared_ptr( T && x )
 {
   return std::make_shared<T>( std::forward<T>(x) );
