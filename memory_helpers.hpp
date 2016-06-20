09cb1883ffcb85b51c73f4725238e5901de30dd8
Ralph Tandetzky <ralph.tandetzky@optimeas.de>
2016-06-20 13:27:21 +0200

    Bugfix: Compile-time errors in to_unique_ptr() and to_shared_ptr() fixed.

--- memory_helpers.hpp
+++ memory_helpers.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <type_traits>
 
 namespace cu
 {
@@ -8,13 +9,13 @@ namespace cu
 template <typename T>
 std::unique_ptr<std::decay_t<T>> to_unique_ptr( T && x )
 {
-  return std::make_unique<T>( std::forward<T>(x) );
+  return std::make_unique<std::decay_t<T>>( std::forward<T>(x) );
 }
 
 template <typename T>
 std::shared_ptr<std::decay_t<T>> to_shared_ptr( T && x )
 {
-  return std::make_shared<T>( std::forward<T>(x) );
+  return std::make_shared<std::decay_t<T>>( std::forward<T>(x) );
 }
 
 } // namespace cu
