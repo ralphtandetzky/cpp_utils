c291cec424c026d88e36eb9ee98a3a10ae25d0cd
Ralph Tandetzky <ralph.tandetzky@gmobis.com>
2017-08-11 09:07:04 +0200

    Implemented transform function taking two tuples and a binary function.

--- meta_programming.hpp
+++ meta_programming.hpp
@@ -97,7 +97,19 @@ namespace detail
                                  F && f,
                                  std::index_sequence<indexes...> )
   {
-    return std::make_tuple( f( std::get<indexes>( std::forward<Tuple>(tuple) ) )... );
+    return std::make_tuple(
+        f( std::get<indexes>( std::forward<Tuple>(tuple) ) )... );
+  }
+
+  template <typename Tuple1, typename Tuple2, typename F, std::size_t ...indexes>
+  decltype(auto) transform_impl( Tuple1 && tuple1,
+                                 Tuple2 && tuple2,
+                                 F && f,
+                                 std::index_sequence<indexes...> )
+  {
+    return std::make_tuple(
+        f( std::get<indexes>( std::forward<Tuple1>(tuple1) ),
+           std::get<indexes>( std::forward<Tuple2>(tuple2) ) )... );
   }
 
 } // namespace detail
@@ -112,6 +124,22 @@ decltype(auto) transform( Tuple && tuple,
         std::forward<Tuple>(tuple),
         std::forward<F>(f),
         make_index_sequence(tuple) );
+}
+
+/// A binary functor is applied to the elements of two tuples and the returned values
+/// are returned in a tuple.
+template <typename Tuple1, typename Tuple2, typename F>
+decltype(auto) transform( Tuple1 && tuple1,
+                          Tuple2 && tuple2,
+                          F && f )
+{
+  static_assert( get_tuple_size(tuple1) == get_tuple_size(tuple2),
+                 "The tuple sized must coincide." );
+  return detail::transform_impl(
+        std::forward<Tuple1>(tuple1),
+        std::forward<Tuple2>(tuple2),
+        std::forward<F>(f),
+        make_index_sequence(tuple1) );
 }
 
 
